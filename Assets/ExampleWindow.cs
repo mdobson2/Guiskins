09b8cf4aee3d9d612723c4808e2cb2747f9e9e76
svc-student <svc-student@student.directory.jccc>
2017-05-01 20:13:57 -0500

    fix

---
+++ Assets/ExampleWindow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class ExampleWindow : EditorWindow {
+
+    public string[] StrArray = { "One", "Two", "Three", "Four" };
+    public int SelectChoice = 0;
+    public string s1 = "";
+    public float f1, f2, f3, f4;
+
+    [MenuItem("Custom Tools/Example Window")]
+    static void DisplayWindow()
+    {
+        GetWindow<ExampleWindow>();
+    }
+
+    void OnGUI()
+    {
+        //the position and rect of the window
+        float colSize = position.width / 10;
+
+        //top
+        GUILayout.Label("Example Window");
+        s1 = GUILayout.TextField(s1);
+
+        GUILayout.Space(10);
+
+        //sliders
+        GUILayout.Label("Sliders");
+        GUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("f1", GUILayout.MaxWidth(colSize/2f));
+            f1 = GUILayout.HorizontalSlider(f1, 0, 10);
+            f1 = EditorGUILayout.FloatField(f1, GUILayout.MaxWidth(colSize));
+            GUILayout.Label("f3", GUILayout.MaxWidth(colSize/2f));
+            f3 = GUILayout.HorizontalSlider(f3, 0, 10);
+            f3 = EditorGUILayout.FloatField(f3, GUILayout.MaxWidth(colSize));
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.Space(10);
+        GUILayout.BeginHorizontal();
+        {
+            GUILayout.Label("f2", GUILayout.MaxWidth(colSize / 2f));
+            f2 = GUILayout.HorizontalSlider(f2, 0, 10);
+            f2 = EditorGUILayout.FloatField(f2, GUILayout.MaxWidth(colSize));
+            GUILayout.Label("f4", GUILayout.MaxWidth(colSize / 2f));
+            f4 = GUILayout.HorizontalSlider(f4, 0, 10);
+            f4 = EditorGUILayout.FloatField(f4, GUILayout.MaxWidth(colSize));
+        }
+        GUILayout.EndHorizontal();
+
+        //selection grid across
+        SelectChoice = GUILayout.SelectionGrid(SelectChoice, StrArray, 4);
+
+        GUILayout.Space(50);
+    }
+}
---
+++ Assets/SampleWindow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SampleWindow : EditorWindow {
+
+    public string[] StrArray = { "One", "Two", "Three", "Four" };
+    public int SelectChoice = 0;
+    public string s1 = "";
+    public float f1, f2, f3, f4;
+    public bool myBool = false;
+
+    public Texture mytexture;
+    public GUISkin mySkin;
+
+    [MenuItem("Custom Tools/Sample Window")]
+    static void DisplayWindow()
+    {
+        GetWindow<SampleWindow>(true);
+    }
+
+    void OnEnable()
+    {
+        mytexture = (Texture) EditorGUIUtility.Load("Blur.jpg");
+        mySkin = (GUISkin)EditorGUIUtility.Load("Skin 1.guiskin");
+    }
+
+    void OnGUI()
+    {
+        //draws the texture background
+        GUI.skin = mySkin;
+        GUI.DrawTexture(new Rect(0, 0, position.width, position.height), mytexture);
+
+        //draws a label
+        GUILayout.Space(10);
+        GUILayout.Label(s1);
+        GUILayout.Space(10);
+        //fills in the label
+        s1 = GUILayout.TextField(s1);
+        GUILayout.Space(10);
+        //slider for the choice
+        SelectChoice = EditorGUILayout.IntSlider(SelectChoice, 0, 10);
+        GUILayout.Space(10);
+        f1 = EditorGUILayout.FloatField(f1, mySkin.GetStyle("textfield"));
+        GUILayout.Space(10);
+        if(GUILayout.Button("Button"))
+        {
+            myBool = !myBool;
+        }
+        GUILayout.Space(10);
+        myBool = EditorGUILayout.Toggle(myBool, mySkin.GetStyle("checkbox"));
+    }
+}
